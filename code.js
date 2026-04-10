04baee9871d979bed48982d32b5efb1ad4bd1cba
majbrittmalmsten053 <majbrittmalmsten@gmail.com>
2026-04-10 14:41:24 +0200

    Create code.js

---
+++ code.js
@@ -0,0 +1,22 @@
+{
+  "CoreSettings": {
+    "MenuKey": "INSERT",
+    "SelfDestructKey": "END",
+    "MaxFPS": 144
+  },
+  "Modules": {
+    "Velocity": {
+      "Enabled": true,
+      "Horizontal": 0.0,
+      "Vertical": 0.0
+    },
+    "Sprint": {
+      "Enabled": true,
+      "Omnidirectional": false
+    },
+    "ChestESP": {
+      "Enabled": true,
+      "Color": [255, 120, 0, 255]
+    }
+  }
+}
